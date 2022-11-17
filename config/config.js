61d0215053ff72c2188a668b7418e13a8dedbd90
Bhramn Saidoung <bhramn@gmail.com>
2022-11-17 16:40:12 +0700

    first commit

---
+++ config/config.js
@@ -0,0 +1,4 @@
+export const API_KEY = `988991dd060549aa92a530cdc916edd2`;
+export const endpoint = `https://newsapi.org/v2/top-headlines`;
+export const category = 'general';
+export const country = 'th'
---
+++ screens/All.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, Dimensions, Linking } from 'react-native';
+import { NativeBaseProvider, FlatList, ScrollView, Divider, Image, Spinner } from 'native-base';
+import { services } from '../services/services';
+import moment from 'moment'
+export default function All() {
+    const [newsData, setNewsData] = useState([])
+    useEffect(() => {
+        services('general')
+            .then(data => {
+                setNewsData(data)
+            })
+            .catch(error => {
+                alert(error)
+            })
+    }, [])
+    return (
+        <NativeBaseProvider>
+                {newsData.length > 1 ? (
+                    <FlatList
+                        data={newsData}
+                        renderItem={({ item }) => (
+                            <View>
+                                <View style={styles.newsContainer}>
+                                    <Image
+                                        width={Dimensions.get('window').width}
+                                        height={250}
+                                        resizeMode={"cover"}
+                                        source={{
+                                            uri: item.urlToImage,
+                                        }}
+                                        alt="Alternate Text"
+                                    />
+                                    <Text style={styles.title}>
+                                        {item.title}
+                                    </Text>
+                                    <Text style={styles.date}>
+                                        {moment(item.publishedAt).format('LLL')}
+                                    </Text>
+                                    <Text style={styles.newsDescription}>
+                                        {item.description}
+                                    </Text>
+                                    <Text style= {{marginTop: 10}}>Source: 
+                                    <Text style={styles.source}
+                                    onPress={() => Linking.openURL(item.url)}>
+                                        {item.url}
+                                    </Text>
+                                    </Text>
+                                </View>
+                                <Divider my={1} bg="#e0e0e0" />
+                            </View>
+                        )}
+                        keyExtractor={(item) => item.title}
+                    />
+                ) : (
+                    <View style={styles.spinner}>
+                        <Spinner color="danger.400" />
+                    </View>
+                )}
+        </NativeBaseProvider>
+    )
+}
+
+const styles = StyleSheet.create({
+    newsContainer: {
+        padding: 10
+    },
+    title: {
+        fontSize: 18,
+        marginTop: 10,
+        fontWeight: "600"
+    },
+    newsDescription: {
+        fontSize: 16,
+        marginTop: 10
+    },
+    date: {
+        fontSize: 14
+    },
+    spinner: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 400
+    },
+    source:{
+        marginTop: 10,
+        color: "#e63946",
+        fontWeight: "bold",
+        fontSize: 12
+    }
+});
